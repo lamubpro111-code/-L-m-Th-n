ca7ebcaf64dca3148a2238e346e4dba94e652b26
lamubpro111-code <lamubpro111@gmail.com>
2026-07-08 03:28:57 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Bố mày là bot, bố mày không nói được♥ ",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 2,
       },
     ],
   },
@@ -91,7 +91,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#fc2ba9",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
