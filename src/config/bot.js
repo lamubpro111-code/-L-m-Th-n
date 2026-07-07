185762d52de2152f79fcce778d31c6e5694677a4
lamubpro111-code <lamubpro111@gmail.com>
2026-07-08 03:30:18 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Bố mày là bot♥ ",
+        name: "Bố mày là bot💜 ",
         // Activity type number (0 = Playing).
         type: 2,
       },
